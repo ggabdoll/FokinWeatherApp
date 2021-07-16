45dff37a98ce0bd660f94628fff77146d177f28b
ggabdoll <ggabdoll0327@naver.com>
2021-07-16 20:51:08 +0900

    Icon & gradient

--- Weather.js
+++ Weather.js
@@ -1,12 +1,25 @@
 import React  from "react";
 import { View, Text, StyleSheet} from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 
 export default function Weather({temp}){
     return( 
-        < View style={styles.container}>
-            <Text>{temp}</Text>
+        <LinearGradient
+            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            style={styles.container}
+        >
+            <View style={styles.halfContainer}>
+          <MaterialCommunityIcons size={96} name="weather-lightning-rainy" 
+            color="white"/>
+          <Text style={styles.temp}>{temp}o</Text>
         </View>
+        <View style={styles.halfContainer} />
+      
+    </LinearGradient>
     );
 }
 
@@ -29,6 +42,15 @@ Weather.PropTypes = {
 const styles = StyleSheet.create({
     container: {
         flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+        fontSize: 42,
+        color: "white"
+    },
+    halfContainer: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center"
     }
