180e435ae925961c2a67fa109bccd93a246baf35
ggabdoll <ggabdoll0327@naver.com>
2021-12-29 19:27:02 +0900

    git test

--- Weather.js
+++ Weather.js
@@ -1,57 +1,63 @@
-import React  from "react";
-import { View, Text, StyleSheet} from "react-native";
-import PropTypes from "prop-types";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
+const weatherOptions = {
+    Haze: {
+        iconName: 'weather-hail',
+        gradient: ['#4DA0B0', '#D39D38'],
+    },
+};
 
-
-export default function Weather({temp}){
-    return( 
-        <LinearGradient
-            colors={["#4c669f", "#3b5998", "#192f6a"]}
-            style={styles.container}
-        >
+export default function Weather({ temp, condition }) {
+    return (
+        <LinearGradient colors={weatherOptions[condition].gradient} style={styles.container}>
+            <StatusBar barStyle="light-content" />
             <View style={styles.halfContainer}>
-          <MaterialCommunityIcons size={96} name="weather-lightning-rainy" 
-            color="white"/>
-          <Text style={styles.temp}>{temp}o</Text>
-        </View>
-        <View style={styles.halfContainer} />
-      
-    </LinearGradient>
+                <MaterialCommunityIcons
+                    size={96}
+                    name={weatherOptions[condition].iconName}
+                    color="white"
+                />
+                <Text style={styles.temp}>{temp}o</Text>
+            </View>
+            <View style={styles.halfContainer} />
+        </LinearGradient>
     );
 }
 
 Weather.PropTypes = {
-    temp:PropTypes.number.isRequired,
+    temp: PropTypes.number.isRequired,
     condition: PropTypes.oneOf([
-        "Thunderstorm",
-        "Drizzle",
-        "Rain",
-        "Snow",
-        "Atmosphere",
-        "Clear",
-        "Clouds",
-        "Haze",
-        "Mist",
-        "Dust"
-      ]).isRequired
+        'Thunderstorm',
+        'Drizzle',
+        'Rain',
+        'Snow',
+        'Atmosphere',
+        'Clear',
+        'Clouds',
+        'Haze',
+        'Mist',
+        'Dust',
+        'test',
+    ]).isRequired,
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center"
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     temp: {
         fontSize: 42,
-        color: "white"
+        color: 'white',
     },
     halfContainer: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
