cd98cc4e6de2dc5823b2a006cbe1bc6b297de49c
ggabdoll <ggabdoll0327@naver.com>
2022-01-10 20:03:17 +0900

    test checkout

--- Weather.js
+++ Weather.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'pink',
+        backgroundColor: 'blue',
         color: 'yellow',
     },
     temp: {
